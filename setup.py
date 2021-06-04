d2f79d57037d31e5f4c84ffe8d75bd1605334a87
Miguel <miguellealw@gmail.com>
2021-06-04 03:58:08 -0500

    Added setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+
+
+# Docs on packaging projects - https://packaging.python.org/tutorials/packaging-projects/
+# Flask docs - https://flask.palletsprojects.com/en/1.1.x/tutorial/install/
+
+# To install - pip install -e .
+setup(
+  name='Web Room',
+  version='0.0.1',
+  packages=find_packages(),
+  include_package_data=True, # this is for getting data in MANIFEST.in
+  zip_safe=False,
+  install_requires=[
+    'flask',
+  ]
+
+)
