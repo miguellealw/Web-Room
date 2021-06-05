a8edfe1511ca9f23ec3bb78a999d36fa398cd116
Miguel <miguellealw@gmail.com>
2021-06-05 11:23:19 -0500

    Added config file for env vars

--- YG_server/__init__.py
+++ YG_server/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 
 
 # TODO: setup postgresql
@@ -11,21 +11,15 @@ def create_app(test_config=None):
   # __name__ is the name of the current python module
   # info on flask object - https://flask.palletsprojects.com/en/2.0.x/api/#flask.Flask
   app = Flask(__name__, instance_relative_config=True)
+  app.config.from_object('config.DevConfig')
+  # app.config.from_object('config.ProdConfig')
 
-  app.config.from_mapping(
-    # TODO: change this to random value when deploying
-    SECRET_KEY='dev',
-    # instance folder is located outside YG-server package and 
-    # can hold local data that shouldn't be commited to version control, like config secrets and DB files
-    DATABASE=os.path.join(app.instance_path, 'yg.sqlite'),
-  )
-
-  if test_config is None:
-    # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent=True)
-  else:
-    # load the test config if passed in
-    app.config.from_mapping(test_config)
+  # if test_config is None:
+  #   # load the instance config, if it exists, when not testing
+  #   app.config.from_pyfile('config.py', silent=True)
+  # else:
+  #   # load the test config if passed in
+  #   app.config.from_mapping(test_config)
 
   # ensure the instance folder exists
   try:
@@ -33,15 +27,19 @@ def create_app(test_config=None):
   except OSError:
     pass
 
-  # a simple page that says hello
-  @app.route('/hello')
-  def hello():
-    return 'Hello, World!'
+  @app.errorhandler(404)
+  def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
+  ## INIT DATABASE
   from . import db
   db.init_app(app)
 
+  ## REGISTER BLUEPRINTS
+  url_version = '/api/v1.0'
   from . import auth
-  app.register_blueprint(auth.bp)
+  from .categories import categories
+  app.register_blueprint(auth.bp, url_prefix=f'{url_version}/auth')
+  app.register_blueprint(categories, url_prefix=f'{url_version}/categories')
 
   return app
---
+++ config.py
@@ -0,0 +1,43 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+baseddir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(baseddir, '.env'))
+
+
+"""
+
+SESSION_COOKIE_NAME=
+PROD_DATABASE_URI=
+DEV_DATABASE_URI=
+
+"""
+
+
+# BASE CONFIG
+class Config:
+  SECRET_KEY = environ.get('SECRET_KEY')
+  FLASK_APP = environ.get('FLASK_APP')
+  FLASK_ENV= environ.get('FLASK_ENV')
+  SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME')
+  STATIC_FOLDER = 'static'
+
+  # [DB_TYPE]+[DB_CONNECTOR]://[USERNAME]:[PASSWORD]@[HOST]:[PORT]/[DB_NAME]
+  SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+  SQLALCHEMY_ECHO = False
+  # disable warning of system resources
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProdConfig(Config):
+  FLASK_ENV = 'production'
+  DEBUG = False
+  TESTING = False
+  DATABASE_URI = environ.get('PROD_DATABASE_URI')
+
+
+class DevConfig(Config):
+  FLASK_ENV = 'development'
+  DEBUG = True
+  TESTING = True
+  DATABASE_URI = environ.get('DEV_DATABASE_URI')
