f243443840c9e9b2f7eea6ac97dca95aea9c972b
Miguel <miguellealw@gmail.com>
2021-06-05 21:39:07 -0500

    POST category now in same function

--- YG_server/categories/routes.py
+++ YG_server/categories/routes.py
@@ -1,18 +1,25 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, abort
 
-# from YG_server.db import get_db
+from .models import Category
 
 categories_bp = Blueprint('categories', __name__)
 
-@categories_bp.route('/', methods=['GET'])
+@categories_bp.route('', methods=['GET', 'POST'])
 def all():
+  if request.method == 'POST':
+    name = request.get_json()
+    if name is None:
+      abort(400)
+
+    # TODO: create category
+    new_category = Category(
+      
+    )
+    return jsonify(name), 201
+
   return jsonify({'message': 'all categoriies'})
 
 @categories_bp.route('/<int:category_id>', methods=['GET'])
 def get_category(category_id):
   return jsonify({'message': f'category: {category_id}'})
 
-
-@categories_bp.route('', methods=['POST'])
-def create_category():
-  return jsonify({'message': 'new category'}), 201
