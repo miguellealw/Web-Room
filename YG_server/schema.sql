f33ec0c68a154ddd0a127d6b0469d6df695e9fb4
Miguel <miguellealw@gmail.com>
2021-06-05 21:41:00 -0500

    some comments

--- YG_server/schema.sql
+++ YG_server/schema.sql
@@ -12,24 +12,26 @@ CREATE TABLE users (
 -- categories created by user for their subscriptions
 CREATE TABLE categories (
   category_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  author_id INTEGER NOT NULL,
+  -- author_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   title TEXT NOT NULL,
 
+  -- reference to user
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
 
 -- channel will relate user and category (user has category, category has channels)
 CREATE TABLE channels (
   channel_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   name TEXT NOT NULL,
 
   fk_category INTEGER NOT NULL,
   fk_user INTEGER NOT NULL,
 
-  -- FOREIGN KEY <name of foreign key in table> REFERENCES <table> <column in table>
+  -- FOREIGN KEY <name of column in table> REFERENCES <other table> <column in other table>
   -- TODO: figure out if composition table is needed
   FOREIGN KEY (fk_category) REFERENCES categories (category_id),
+
+  -- TODO: figure out if relating to user is necessary b/c channels will be fetchable from the YouTube API
   FOREIGN KEY (fk_user) REFERENCES users (user_id)
 );
