b8703bd143f060770a52b3f14aaea7fd894cf09d
Miguel <miguellealw@gmail.com>
2021-06-07 10:15:57 -0500

    implemented categories GET and POST

--- YG_server/categories/routes.py
+++ YG_server/categories/routes.py
@@ -1,23 +1,40 @@
 from flask import Blueprint, jsonify, request, abort
+from datetime import datetime as dt
 
-from .models import Category
+from .models import db, Category
 
 categories_bp = Blueprint('categories', __name__)
 
 @categories_bp.route('', methods=['GET', 'POST'])
 def all():
   if request.method == 'POST':
-    name = request.get_json()
-    if name is None:
-      abort(400)
+    created_category = request.get_json()
+    name = created_category['name']
+    user_id = created_category['user_id']
 
-    # TODO: create category
-    # new_category = Category(
+    if name is None or user_id is None:
+      abort(400, description="Name not passed from client")
 
-    # )
-    return jsonify(name), 201
+    new_category = Category(
+      name=name,
+      user_id=user_id,
+      created=dt.now()
+    )
 
-  return jsonify({'message': 'all categoriies'})
+    db.session.add(new_category)
+    db.session.commit()
+
+    if new_category is None:
+      abort(409, description="Category could not be created")
+
+    return jsonify(f'category created: {new_category.name}'), 201
+
+
+  categories = Category.query.all()
+  if categories is None:
+    abort(404, description="No categories available")
+
+  return jsonify(str(categories)), 200
 
 @categories_bp.route('/<int:category_id>', methods=['GET'])
 def get_category(category_id):
