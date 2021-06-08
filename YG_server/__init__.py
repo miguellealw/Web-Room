29181d0e1e7f7eaf74b9c2474c913b36e0890f1e
Miguel <miguellealw@gmail.com>
2021-06-08 16:13:06 -0500

    moved register and login to auth

--- YG_server/__init__.py
+++ YG_server/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import HTTPException
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def resource_not_found(e):
   return jsonify(error=str(e)), 404
@@ -16,10 +18,7 @@ def default_handler(e):
 
   return jsonify({"error": "Non-HTTP Error"}), 500
 
-def create_app(test_config=None):
-  # __name__ is the name of the current python module
-  # info on flask object - https://flask.palletsprojects.com/en/2.0.x/api/#flask.Flask
-
+def create_app():
   ## CREATE APP AND LOAD CONFIG
   app = Flask(__name__, instance_relative_config=True)
   app.config.from_object('config.DevConfig')
@@ -31,20 +30,20 @@ def create_app(test_config=None):
   app.register_error_handler(Exception, default_handler)
 
   ## REGISTER PLUGINS - make globally accessible to other parts of app
-  # register db
-  db.init_app(app)
+  db.init_app(app) # register db
+  login_manager.init_app(app) # register login_manager
 
   ## REGISTER BLUEPRINTS
   with app.app_context():
-    from . import auth
+    from .auth import routes as auth
     from .channels import routes as channels
     from .users import routes as users
     from .categories import routes as categories
 
     url_version = '/api/v1.0'
-    # app.register_blueprint(auth.bp, url_prefix=f'{url_version}/auth')
     app.register_blueprint(categories.categories_bp, url_prefix=f'{url_version}/categories')
     app.register_blueprint(channels.channels_bp, url_prefix=f'{url_version}/channels')
     app.register_blueprint(users.users_bp, url_prefix=f'{url_version}/users')
+    app.register_blueprint(auth.bp, url_prefix=f'{url_version}/auth')
 
   return app
---
+++ YG_server/auth/routes.py
@@ -0,0 +1,48 @@
+from flask import jsonify, request, abort
+from datetime import datetime as dt
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from YG_server.auth import bp
+from YG_server.users.models import User
+
+from YG_server.users.models import db, User
+
+
+@bp.route('/register', methods=['POST'])
+def register():
+  if request.method == 'POST':
+    user = request.get_json()
+    username = user['username']
+    password = user['password']
+    error = None
+
+    # Check if user or password were not provided.
+    # Otherwise check if user already exists in db
+    if not username:
+      error = 'Username is required.'
+    elif not password:
+      error = 'Password is required.'
+    elif User.query.filter(User.username == username).first() is not None:
+      error = f'User \'{username}\' is already registered.'
+
+    # Return if error is found
+    if error is not None:
+      abort(409, description=error)
+
+    # if not error add user to db
+    new_user = User(
+      username = username,
+      password = generate_password_hash(password),
+      created = dt.now()
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"New User": new_user.username})
+
+
+@bp.route('/login', methods=['GET'])
+def login():
+  # TODO: use flask-login, flask-praetorian, or Auth0
+  return jsonify(message="LOGIN NOT IMPLMENTED")
+  pass
