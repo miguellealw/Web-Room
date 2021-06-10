46113589a6f2c985a5dfada8cbdf01eb9ee42c2f
Miguel <miguellealw@gmail.com>
2021-06-09 22:32:28 -0500

    user endpoints now require login

--- YG_server/api/users.py
+++ YG_server/api/users.py
@@ -5,83 +5,78 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from YG_server.models import db, User, Category
 from YG_server.api import bp
 
-# users_bp = Blueprint('User', __name__, url_prefix='/users')
+from YG_server.auth.routes import load_user
 
-@bp.route('/users', methods=['GET'])
-def get_user():
-  user_id = request.args.get('id')
+from flask_login import login_required, current_user as fl_current_user
+
+@bp.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+  # user_id = request.args.get('id')
   user_found = User.query.get(user_id)
 
   if user_found is None:
-    abort(404)
+    abort(404, description='User does not exist')
 
   return jsonify({"username": user_found.username})
 
 @bp.route('/users/current_user', methods=['GET'])
+@login_required
 def current_user():
-  # TODO: get jwt token and find user
-  id = request.get_json()['user_id']
-  user_found = User.query.get(id)
-
-  if user_found is None:
-    abort(404, description="User not found")
-
-  # channels = user_found.channels
-  # if channels is None:
-  #   abort(404, description="User has no channels")
-
-  # categories = user_found.categories
-  # if categories is None:
-  #   abort(404, description="User has no categories")
-
-  # TODO: get channels of current user
   return jsonify({
-    "username": user_found.username, 
+    "username": fl_current_user.username, 
   })
 
-@bp.route('/current_user/channels', methods=['GET'])
-def get_user_channels():
-  id = request.get_json()['user_id']
-  user_found = User.query.get(id)
 
+@bp.route('/users/current_user/channels', methods=['GET'])
+@login_required
+def get_user_channels():
+  # username = request.get_json()['username']
+
+  user_found = User.query.get(fl_current_user.id)
   if user_found is None:
     abort(404, description="User not found")
 
   channels = user_found.channels
   if channels is None:
-    abort(404, description="User has no channels")
+    abort(404, description="User channels could not be loaded")
 
   return jsonify({
     "channels": str(channels),
   })
 
 @bp.route('/users/current_user/categories', methods=['GET'])
+@login_required
 def get_user_categories():
-  id = request.get_json()['user_id']
-  user_found = User.query.get(id)
+  # username = request.get_json()['username']
 
+  user_found = User.query.get(fl_current_user.id)
   if user_found is None:
     abort(404, description="User not found")
 
   categories = user_found.categories
   if categories is None:
-    abort(404, description="User has no categories")
+    abort(404, description="User categories could not be loaded")
 
   return jsonify({
     "categories": str(categories),
   })
 
 @bp.route('/users/current_user/categories/<int:category_id>', methods=['GET'])
+@login_required
 def get_user_category(category_id):
-  user_id = request.get_json()['user_id']
-  category = Category.query.get(category_id)
+  user_found = User.query.get(fl_current_user.id)
+  if user_found is None:
+    abort(404, description="User not found")
 
+  # TODO consider using user_found.categories
+
+  category = Category.query.get(category_id)
   if category is None:
     abort(404, description=f"Category does not exist")
 
   # check if user owns category
-  if category.user_id != int(user_id):
-    abort(404, description=f"User does not own category with id={category_id}")
+  if category.user_id != int(user_found.id):
+    abort(404, description=f"User does not own provided category")
 
   return jsonify({
     "category": str(category),
