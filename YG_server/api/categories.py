a01d2716dd049bb53835a6486391b19abe42766d
Miguel <miguellealw@gmail.com>
2021-06-10 19:13:39 -0500

    removed unecessary code

--- YG_server/api/categories.py
+++ YG_server/api/categories.py
@@ -4,12 +4,9 @@ from flask.helpers import url_for
 from YG_server.models import db, Category, User
 from YG_server.api import bp
 
-from flask_login import login_required, current_user as fl_current_user
-
 from YG_server.schemas import category_schema, categories_schema
 
 @bp.route('/categories', methods=['GET', 'POST'])
-@login_required
 def get_categories():
   categories = Category.query.all()
   if categories is None:
@@ -19,10 +16,7 @@ def get_categories():
 
 
 @bp.route('/categories/<int:category_id>', methods=['GET', 'DELETE', 'PUT'])
-@login_required
 def get_category(category_id):
-
-  # TODO: only get category if user owns it
   found_category = Category.query.get(category_id)
   if found_category is None:
     abort(404, description="Category does not exist")
