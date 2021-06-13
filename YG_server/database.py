9db6abdd363de5aa3fe003b005d7bc8cfef5da5c
Miguel <miguellealw@gmail.com>
2021-06-12 22:49:18 -0500

    removed comments

--- YG_server/database.py
+++ YG_server/database.py
@@ -1,8 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy as sa
 
 db = sa()
-
-# def db_init(conn_string):
-# 	engine = sa.create_engine(conn_string or conn_string)
-# 	sa.Metadata().create_all(engine)
-# 	connection = engine.connect()
