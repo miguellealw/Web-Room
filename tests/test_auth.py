8a84c2e70608144fd755f047eda191301a34d4f4
Miguel <miguellealw@gmail.com>
2021-06-15 00:57:53 -0500

    test logout and login

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,13 +3,35 @@ from datetime import datetime as dt
 from tests.base import BaseTestCase
 from YG_server.models import db, Category, User, Channel
 
+# TODO: test failed logins
+
 class TestAuth(BaseTestCase):
 	def test_login(self):
-		pass
+
+		# ARRANGE
+		self.client.post('/auth/v1.0/register', json={
+			"username": "testuser",
+			"email": "test@email.com",
+			"password": "Password123@",
+			"confirm_password": "Password123@"
+		})
+		self.client.get('/auth/v1.0/logout')
+
+		# ACT
+		user_res = self.client.get('/auth/v1.0/login', json={
+			"username": "testuser",
+			"password": "Password123@"
+		})
+		user = json.loads(user_res.data)
+
+		# ASSERT
+		self.assertEqual(user_res.status_code, 200)
+		self.assertEqual(user["flash"], "User logged in")
+		self.assertEqual(user["username"], "testuser")
+
 
 	def test_register(self):
 		user_res = self.client.post('/auth/v1.0/register', json={
-			"id": 1,
 			"username": "testuser",
 			"email": "test@email.com",
 			"password": "Password123@",
@@ -17,12 +39,29 @@ class TestAuth(BaseTestCase):
 		})
 		user = json.loads(user_res.data)
 
+		self.assertEqual(user_res.status_code, 201)
 		self.assertEqual(user["email"], "test@email.com")
 		self.assertEqual(user["username"], "testuser")
-		self.assertEqual(user["id"], 1)
+		# self.assertEqual(user["id"], 2)
 
 		self.assertNotIn("hashed_password", user)
 
 
 	def test_logout(self):
-		pass
+		user_res = self.client.post('/auth/v1.0/register', json={
+			"id": 1,
+			"username": "testuser",
+			"email": "test@email.com",
+			"password": "Password123@",
+			"confirm_password": "Password123@"
+		})
+		# user = json.loads(user_res.data)
+
+		logout_res = self.client.get('/auth/v1.0/logout')
+		logout = json.loads(logout_res.data)
+
+		self.assertEqual(logout_res.status_code, 200)
+		self.assertEqual(logout["flash"], "User logged out")
+
+
+
