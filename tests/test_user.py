3db58ed4a4576e2ac28465436588173f111f75ab
Miguel <miguellealw@gmail.com>
2021-06-15 00:39:58 -0500

    added more category tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -20,8 +20,6 @@ class TestUser(BaseTestCase):
 		})
 		self.user = json.loads(self.user_res.data)
 
-
-
 	# def tearDown(self):
 		# super().teardDown()
 		# pass
@@ -39,6 +37,38 @@ class TestUser(BaseTestCase):
 
 		self.assertEqual(self.user['username'], "testuser")
 		self.assertEqual(category['name'], "Test Category")
+
+	def test_create_user_specified_category_404(self):
+		## ACT
+		category_res = self.client.get('/api/v1.0/users/current_user/categories/1')
+		category = json.loads(category_res.data)
+
+		## ASSERT
+		self.assertEqual(category_res.status_code, 404)
+		self.assertEqual(category["error"], '404 Not Found: Category does not exist')
+
+	
+	# * GET /api/v1.0/users/current_user/categories
+	def test_get_user_categories(self):
+		# ARRANGE
+		new_category = Category(id=1, name="Test Category", user_id=self.user["id"], created_at=dt.now())
+		new_category_2 = Category(id=2, name="Test Category 2", user_id=self.user["id"], created_at=dt.now())
+		new_category_3 = Category(id=3, name="Test Category 3", user_id=self.user["id"], created_at=dt.now())
+
+		db.session.add(new_category)
+		db.session.add(new_category_2)
+		db.session.add(new_category_3)
+		db.session.commit()
+
+		# ACT
+		categories_res = self.client.get('/api/v1.0/users/current_user/categories')
+		categories = json.loads(categories_res.data)
+
+		# ASSERT
+		self.assertEqual(categories_res.status_code, 200)
+		self.assertEqual(len(categories), 3)
+
+
 	
 	# * GET /api/v1.0/users/current_user/categories/<category_id>
 	def test_get_user_specified_category(self):
@@ -56,6 +86,91 @@ class TestUser(BaseTestCase):
 		self.assertEqual(new_category.name, category['name'])
 		self.assertEqual(category["_owner"], f'/api/v1.0/users/{self.user["id"]}')
 
+	def test_get_user_specified_category_404(self):
+		## ACT
+		category_res = self.client.get('/api/v1.0/users/current_user/categories/1')
+		category = json.loads(category_res.data)
+
+		## ASSERT
+		self.assertEqual(category_res.status_code, 404)
+		self.assertEqual(category["error"], '404 Not Found: Category does not exist')
+
+	# * POST /api/v1.0/users/current_user/categories/<category_id>/add_channel
+	def test_add_channel_to_category(self):
+		# Create category to add channel to
+		new_category = Category(id=1, name="Test Category", user_id=self.user["id"], created_at=dt.now())
+		db.session.add(new_category)
+		db.session.commit()
+
+		# ACT
+		channel_res = self.client.post('/api/v1.0/users/current_user/categories/1/add_channel', json={
+			"name": "Test YouTube Channel",
+			"yt_channel_id": "test_id_test"
+		})
+		channel = json.loads(channel_res.data)
+
+		self.assertEqual(channel_res.status_code, 200)
+		self.assertEqual(channel["name"], "Test YouTube Channel")
+		self.assertEqual(channel["yt_channel_id"], "test_id_test")
+		self.assertIn("_links", channel)
+
+
+	# * GET /api/v1.0/users/current_user/channels
+	def test_get_user_channels(self):
+		# ARRANGE
+		# Create Category
+		new_category = Category(id=1, name="Test Category", user_id=self.user["id"], created_at=dt.now())
+		db.session.add(new_category)
+		db.session.commit()
+
+		# Create Channels
+		self.client.post('/api/v1.0/users/current_user/categories/1/add_channel', json={
+			"name": "Test YouTube Channel",
+			"yt_channel_id": "test_id_test"
+		})
+		self.client.post('/api/v1.0/users/current_user/categories/1/add_channel', json={
+			"name": "Test YouTube Channel 2",
+			"yt_channel_id": "test_id_test_2"
+		})
+
+		# ACT
+		channels_res = self.client.get('/api/v1.0/users/current_user/channels')
+		channels = json.loads(channels_res.data)
+
+		# ASSERT
+		self.assertEqual(channels_res.status_code, 200)
+		self.assertEqual(channels[0]["name"], "Test YouTube Channel")
+		self.assertEqual(channels[0]["yt_channel_id"], "test_id_test")
+
+		self.assertEqual(channels[1]["name"], "Test YouTube Channel 2")
+		self.assertEqual(channels[1]["yt_channel_id"], "test_id_test_2")
+		self.assertEqual(len(channels), 2)
+
+	# * GET /api/v1.0/users/current_user/channels/<channel_id>
+	def test_get_user_specified_channel(self):
+		# ARRANGE
+		# Create Category
+		new_category = Category(id=1, name="Test Category", user_id=self.user["id"], created_at=dt.now())
+		db.session.add(new_category)
+		db.session.commit()
+
+		ch_res = self.client.post('/api/v1.0/users/current_user/categories/1/add_channel', json={
+			"id": 1,
+			"name": "Test YouTube Channel",
+			"yt_channel_id": "test_id_test"
+		})
+
+		# ACT
+		channel_res = self.client.get('/api/v1.0/users/current_user/channels/4')
+		channel = json.loads(channel_res.data)
+
+		# ASSERT
+		self.assertEqual(channel_res.status_code, 200)
+		self.assertEqual(channel["name"], "Test YouTube Channel")
+		self.assertEqual(channel["yt_channel_id"], "test_id_test")
+		self.assertIn("_links", channel)
+
+
 	# * GET /api/v1.0/users/current_user
 	def test_get_current_user(self):
 		current_user_res = self.client.get('/api/v1.0/users/current_user')
@@ -67,15 +182,3 @@ class TestUser(BaseTestCase):
 	def test_get_user(self):
 		pass
 
-	# * GET /api/v1.0/users/current_user/channels
-	def test_get_user_channels(self):
-		pass
-
-
-	# * GET /api/v1.0/users/current_user/channels/<channel_id>
-	def test_get_user_specified_channel(self):
-		pass
-
-	# * POSt /api/v1.0/users/current_user/categories/<category_id>/add_channel
-	def test_add_channel_to_category(self):
-		pass
