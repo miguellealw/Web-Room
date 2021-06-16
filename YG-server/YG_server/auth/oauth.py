d8cdefd33c963a6d4df3ec4fa71716901fd8feb8
Miguel <miguellealw@gmail.com>
2021-06-16 01:50:51 -0500

    Setup oauth for youtube

---
+++ YG-server/YG_server/auth/oauth.py
@@ -0,0 +1,51 @@
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+from httplib2 import REDIRECT_CODES
+from flask import session
+
+from os import environ, path
+from dotenv import load_dotenv
+
+baseddir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(baseddir, '.env'))
+CLIENT_SECRETS_FILE = environ.get('CLIENT_SECRET_FILENAME')
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account.
+SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
+REDIRECT_URI = ''
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+def get_authenticated_service():
+	# flow docs - https://google-auth-oauthlib.readthedocs.io/en/latest/reference/google_auth_oauthlib.flow.html
+  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+  # credentials = flow.run_console()
+  credentials = google.oauth2.credentials.Credentials(**session['credentials'])
+  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+
+# This method calls the API's youtube.subscriptions.insert method to add a
+# subscription to the specified channel.
+# def add_subscription(youtube, channel_id):
+#   add_subscription_response = youtube.subscriptions().insert(
+#     part='snippet',
+#     body=dict(
+#       snippet=dict(
+#         resourceId=dict(
+#           channelId=channel_id
+#         )
+#       )
+#     )).execute()
+
+#   return add_subscription_response['snippet']['title']
+
+def get_subscriptions(service, **kwargs):
+	list_subscriptions_response = service.subscriptions().list(
+		# part='snippet'
+		**kwargs
+	).execute()
+
+	return list_subscriptions_response
--- YG-server/YG_server/auth/routes.py
+++ YG-server/YG_server/auth/routes.py
@@ -1,4 +1,5 @@
-from flask import jsonify, request, abort
+from YG_server.auth.oauth import CLIENT_SECRETS_FILE, SCOPES
+from flask import jsonify, request, abort, url_for, session, redirect
 from datetime import datetime
 from flask_login import login_required, logout_user, current_user, login_user
 from YG_server.auth import bp
@@ -6,6 +7,13 @@ from YG_server.models import db, User
 from YG_server import login_manager
 from YG_server.schemas import UserSchema, user_schema
 from marshmallow import ValidationError
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+from os import environ, path
+from dotenv import load_dotenv
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -96,3 +104,64 @@ def login():
 def logout():
   logout_user()
   return jsonify({"flash": "User logged out"})
+
+### YOUTUBE API / GOOGLE OAUTH
+
+baseddir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(baseddir, '.env'))
+# CLIENT_SECRETS_FILE = environ.get('CLIENT_SECRET_FILENAME')
+# SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
+
+
+@bp.route('/authorize')
+def authorize():
+  # Create a flow instance to manage the OAuth 2.0 Authorization Grant Flow
+  # steps.
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES)
+  flow.redirect_uri = url_for('auth.oauth2callback', _external=True)
+  authorization_url, state = flow.authorization_url(
+      # This parameter enables offline access which gives your application
+      # both an access and refresh token.
+      access_type='offline',
+      # This parameter enables incremental auth.
+      include_granted_scopes='true')
+
+
+  # Store the state in the session so that the callback can verify that
+  # the authorization server response.
+  session['state'] = state
+
+  return redirect(authorization_url)
+
+
+@bp.route('/oauth2callback')
+def oauth2callback():
+  # Specify the state when creating the flow in the callback so that it can
+  # verify the authorization server response.
+  state = session['state']
+  flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+  flow.redirect_uri = url_for('auth.oauth2callback', _external=True)
+
+  # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+  authorization_response = request.url
+  flow.fetch_token(authorization_response=authorization_response)
+
+  # Store the credentials in the session.
+  # ACTION ITEM for developers:
+  #     Store user's access and refresh tokens in your data store if
+  #     incorporating this code into your real app.
+  credentials = flow.credentials
+  print("CREDENTIALS", credentials)
+  session['credentials'] = {
+      'token': credentials.token,
+      'refresh_token': credentials.refresh_token,
+      'token_uri': credentials.token_uri,
+      'client_id': credentials.client_id,
+      'client_secret': credentials.client_secret,
+      'scopes': credentials.scopes
+  }
+
+  return jsonify({"message": "AUTHED WITH YOUTUBE"})
+  # return redirect(url_for('api.get_categories'))
