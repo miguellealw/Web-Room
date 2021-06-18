91c8833bba6c6759cbcb8b0e0b7308f04fa4b37d
Miguel <miguellealw@gmail.com>
2021-06-18 00:52:55 -0500

    name changes

--- YG-server/YG_server/decorators.py
+++ YG-server/YG_server/decorators.py
@@ -4,11 +4,11 @@ from YG_server.auth.oauth import get_authenticated_service
 
 def yt_auth_required(f):
 	@wraps(f)
-	def decorated_function(*args, **kwargs):
+	def route_handler(*args, **kwargs):
 		# Check if user is authed by youtube
 		if 'credentials' not in session:
-			return redirect('authorize')
+			return redirect('auth.authorize')
 
 		yt_client = get_authenticated_service()
 		return f(yt_client, *args, **kwargs)
-	return decorated_function
+	return route_handler
