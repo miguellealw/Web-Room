a46a62e29aa4c9128c87bebdf24567befc0d8534
Miguel <miguellealw@gmail.com>
2021-06-20 20:48:19 -0500

    added categories page

---
+++ yg-client/pages/categories.tsx
@@ -0,0 +1,14 @@
+import AuthedLayout from './authed_layout'
+
+
+const Categories : React.FC = () => {
+
+	return (
+		<AuthedLayout>
+			<h1>Your Categories</h1>
+		</AuthedLayout>
+	)
+
+}
+
+export default Categories
