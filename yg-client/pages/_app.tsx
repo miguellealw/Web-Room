a4e75f24fe3adcff870676b1e854239db7242ac4
Miguel <miguellealw@gmail.com>
2021-06-20 20:48:40 -0500

    auth provider

--- yg-client/pages/_app.tsx
+++ yg-client/pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ProvideAuth } from '../utils/auth/useAuth'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ProvideAuth>
+      <Component {...pageProps} />
+    </ProvideAuth>
+  )
 }
 export default MyApp
