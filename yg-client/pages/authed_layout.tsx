1e135c9cf29ba12ebb0ad60f479d934f3b084f25
Miguel <miguellealw@gmail.com>
2021-06-20 23:57:13 -0500

    tooltip has effect solid

--- yg-client/pages/authed_layout.tsx
+++ yg-client/pages/authed_layout.tsx
@@ -36,7 +36,7 @@ const AuthedLayout : FC = ({ children }) => {
 								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 		<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
 	</svg>
-								<ReactTooltip />
+								<ReactTooltip effect="solid"/>
 							</a>
 						</Link>
 					</li>
@@ -46,7 +46,7 @@ const AuthedLayout : FC = ({ children }) => {
 								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
 </svg>
-								<ReactTooltip />
+								<ReactTooltip effect="solid"/>
 							</a>
 						</Link>
 					</li>
@@ -55,7 +55,7 @@ const AuthedLayout : FC = ({ children }) => {
 							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
 </svg>
-							<ReactTooltip />
+							<ReactTooltip effect="solid"/>
 						</button>
 					</li>
 				</ul>
