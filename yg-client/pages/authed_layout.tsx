1f519a88f98a662ca4f8abd55034b3fe96eb28f9
Miguel <miguellealw@gmail.com>
2021-06-21 00:20:03 -0500

    added heroicons

--- yg-client/pages/authed_layout.tsx
+++ yg-client/pages/authed_layout.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { useRouter } from "next/router"
 import { FC } from "react"
 import ReactTooltip from 'react-tooltip'
+import { ViewGridIcon, CollectionIcon, LogoutIcon } from '@heroicons/react/outline'
 
 import useUser from '../utils/auth/useUser'
 import { AuthApi } from './api/auth'
@@ -33,9 +34,7 @@ const AuthedLayout : FC = ({ children }) => {
 					<li className="my-10">
 						<Link href="/categories" passHref>
 							<a data-tip="Your Categories">
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-		<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-	</svg>
+								<ViewGridIcon className="h-5 w-5"/>
 								<ReactTooltip effect="solid"/>
 							</a>
 						</Link>
@@ -43,18 +42,14 @@ const AuthedLayout : FC = ({ children }) => {
 					<li className="my-10">
 						<Link href="/channels">
 							<a data-tip="Your Subscriptions">
-								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
-</svg>
+								<CollectionIcon className="h-5 w-5"/>
 								<ReactTooltip effect="solid"/>
 							</a>
 						</Link>
 					</li>
-					<li className="absolute bottom-2">
+					<li className="absolute bottom-5">
 						<button data-tip="Log Out" onClick={handleLogout}>
-							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-</svg>
+							<LogoutIcon className="h-5 w-5"/>
 							<ReactTooltip effect="solid"/>
 						</button>
 					</li>
