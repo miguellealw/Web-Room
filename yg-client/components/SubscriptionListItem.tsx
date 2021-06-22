bda86f1438c1c37a2606c9607e8e397205bb2702
Miguel <miguellealw@gmail.com>
2021-06-22 00:44:37 -0500

    Created grid view for subs

--- yg-client/components/SubscriptionListItem.tsx
+++ yg-client/components/SubscriptionListItem.tsx
@@ -14,8 +14,41 @@ const SubscriptionListItem : React.FC<SubscriptionListItem> = ({
 	channelId
 }) => {
 
+	// return (
+	// 	<div className="bg-gray-100 p-4 my-4 flex items-center">
+	// 		<div className="w-20 rounded-full overflow-hidden">
+	// 			<Image 
+	// 				src={thumbnail} 
+	// 				alt={`${name}'s thumbnail`} 
+	// 				className="w-full h-full object-cover object-center"
+	// 				width={200}
+	// 				height={200}
+	// 			/>
+	// 		</div>
+	// 		<div className="flex flex-col ml-2">
+	// 			<span className="font-bold">
+	// 				{name}
+	// 			</span>
+
+	// 			<a 
+	// 				href={`https://www.youtube.com/channel/${channelId}`} 
+	// 				// target="_blank" 
+	// 				className="text-xs text-gray-400 hover:underline"
+	// 			>
+	// 				Go to Channel
+	// 			</a>
+
+	// 			{/* {
+	// 				description !== "" ? 
+	// 					description : 
+	// 					(<span className="italic text-gray-400">No description available</span>)
+	// 			} */}
+	// 		</div>
+	// 	</div>
+	// )
+
 	return (
-		<div className="bg-gray-100 p-4 my-4 flex items-center">
+		<div className="bg-gray-100 p-4 flex items-center rounded-md hover:bg-gray-200">
 			<div className="w-20 rounded-full overflow-hidden">
 				<Image 
 					src={thumbnail} 
