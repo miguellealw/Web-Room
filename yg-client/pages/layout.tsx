eb03048a4dcab10c0d66c19708997a7fc4e9f3d5
Miguel <miguellealw@gmail.com>
2021-06-22 00:28:20 -0500

    updated layout

--- yg-client/pages/layout.tsx
+++ yg-client/pages/layout.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link"
 import React, { FC } from "react"
+import Logo from "../components/Logo"
 
 
 const Layout : FC = ({children}) => {
 
 	return (
 		<div>
-			<nav className="w-full text-center py-5 absolute">
+			<nav className="w-full py-5 absolute flex justify-center z-20">
 				<Link href="/" passHref>
-					<a>
-						<h1 className="font-bold">YouTube+</h1>
+					<a className="flex items-center">
+						{/* <Logo className="w-7 h-7 mr-2"></Logo> */}
+						<h1 className="font-bold z-10 relative">YouTube+</h1>
 					</a>
 				</Link>
 			</nav>
