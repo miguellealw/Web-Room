2b1bd0f78605402d42fa911fc430a4057349513b
Miguel <miguellealw@gmail.com>
2021-06-21 20:23:09 -0500

    categories api

---
+++ yg-client/pages/api/categories.ts
@@ -0,0 +1,70 @@
+import { AxiosResponse } from "axios";
+import { Api } from "./api";
+import { Category } from "./types";
+
+type CategoryResponse = {
+	kind: string,
+	categories?: Category[] | [] | null
+	category?: Category | null
+	errorMessage: any
+};
+
+export class CategoryApi extends Api {
+	async getUserCategories() : Promise<CategoryResponse> {
+		try {
+			const response: AxiosResponse<any> = await this.axios.get('/api/v1.0/users/current_user/categories')
+
+			return {
+				kind: "ok",
+				categories: response.data,
+				errorMessage: null
+			}
+		} catch(err) {
+			return { 
+				kind: "bad-data", 
+				categories: null, 
+				errorMessage: err
+			}
+		}
+	}
+
+	async getUserCategoryById(id : number) : Promise<CategoryResponse> {
+		try {
+			const response: AxiosResponse<any> = await this.axios.get(`/api/v1.0/users/current_user/categories/${id}`)
+
+			return {
+				kind: "ok",
+				category: response.data,
+				errorMessage: null
+			}
+		} catch(err) {
+			return { 
+				kind: "bad-data", 
+				category: null, 
+				errorMessage: err
+			}
+		}
+	}
+
+
+	async createCategory(name : string) : Promise<CategoryResponse> {
+		try {
+			const response: AxiosResponse<any> = await this.axios.post(`/api/v1.0/users/current_user/categories`, {
+				name
+			})
+
+			return {
+				kind: "ok",
+				category: response.data,
+				errorMessage: null
+			}
+		} catch(err) {
+			return { 
+				kind: "bad-data", 
+				category: null, 
+				errorMessage: err
+			}
+		}
+
+	}
+}
