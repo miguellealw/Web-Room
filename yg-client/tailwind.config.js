6b222c660e5a26075ecb2c014b710641d80bd9c8
Miguel <miguellealw@gmail.com>
2021-06-23 05:36:11 -0500

    font family is inter

--- yg-client/tailwind.config.js
+++ yg-client/tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ['Inter', 'system-ui', 'sans-serif'],
+      body: ['Inter', 'system-ui', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
