795eb8fb752419a638324b3f984bb83b24c945b3
Miguel <miguellealw@gmail.com>
2021-06-26 05:48:04 -0500

    added global swr config

--- yg-client/pages/_app.tsx
+++ yg-client/pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
+import { SWRConfig } from 'swr'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <SWRConfig value={{
+      dedupingInterval: 5000
+    }}>
+      <Component {...pageProps} />
+
+    </SWRConfig>
   )
 }
 export default MyApp
