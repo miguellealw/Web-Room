0c127ce7c3a3c49dfb8145f05e15a4ed871205c7
Miguel <miguellealw@gmail.com>
2021-06-26 20:47:42 -0500

    Moved isLoading in component

--- yg-client/components/DashboardNavigation.tsx
+++ yg-client/components/DashboardNavigation.tsx
@@ -1,27 +1,26 @@
+import { memo } from 'react'
 import { CollectionIcon, FolderIcon, FolderOpenIcon, LogoutIcon } from "@heroicons/react/outline"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import ReactTooltip from "react-tooltip"
 import { AuthApi } from '../pages/api/auth'
+import { CategoryApi } from "../pages/api/categories"
 import useUser from "../utils/auth/useUser"
 import useCategories from "../utils/useCategories"
+import LoadingText from "./LoadingText"
 import Logo from "./Logo"
 
 
 const DashboardNavigation = () => {
-	const {mutateUser} = useUser()
+	const { mutateUser } = useUser()
 	const router = useRouter()
 	const {data, error, isLoading} = useCategories()
+	// console.log("NAV RERENDERDX")
 
 	if(error) return <div>Error loading page...</div>
 
-	if(isLoading) {
-		return (
-			<div>Loading categories...</div>
-		)
-	}
-
 	const handleLogout = async () => {
+		mutateUser()
 		const api = new AuthApi();
 		api.setup();
 		const response = await api.logout()
@@ -62,21 +61,23 @@ const DashboardNavigation = () => {
 			{/* Categories Section */}
 			<div className="border-t-2 border-b-2 h-full border-gray-700 w-full flex flex-col">
 				<span className="mt-3 ml-5 font-medium text-gray-400">Your Categories</span>
-				<ul className="mt-3">
-					{ data?.categories?.length === 0 
-						? <span>No categories available</span> :
-						data?.categories?.map((category, index) => (
-							<Link href={`/categories/${category.id}`} passHref key={category.id}>
-								<a>
-									<li className="pl-5 py-2 hover:bg-gray-700 flex">
-										<FolderOpenIcon className="w-5 h-5 mr-2" />
-										{category.name}
-									</li>
-								</a>
-							</Link>
-						))
-					}
-				</ul>
+				{isLoading ? (<LoadingText>Loading Categories...</LoadingText>) : (
+					<ul className="mt-3">
+						{ data?.categories?.length === 0 
+							? <span>No categories available</span> :
+							data?.categories?.map((category, index) => (
+								<Link href={`/categories/${category.id}`} passHref key={category.id}>
+									<a>
+										<li className="pl-5 py-2 hover:bg-gray-700 flex">
+											<FolderOpenIcon className="w-5 h-5 mr-2" />
+											{category.name}
+										</li>
+									</a>
+								</Link>
+							))
+						}
+					</ul>
+				)}
 			</div>
 
 			{/* Log Out */}
@@ -89,4 +90,22 @@ const DashboardNavigation = () => {
 	)
 }
 
+// TODO: since the call is made from the server the cookies are not sent w/ the request, so the endpoint will
+// assume the user is not authed
+
+// export async function getServerSideProps() {
+// 	const api = new CategoryApi();
+// 	api.setup();
+//   const res = await api.getUserCategories()
+
+// 	return { props: {categories: res.categories} }
+// }
+
+
+// TODO: figure out how to avoid rerenders
 export default DashboardNavigation
+// export default memo(
+// 	DashboardNavigation, 
+// 	(prevProps, nextProps) => 
+// 		prevProps.categoriesData.data.categories.length === nextProps.categoriesData.data.categories.length
+// )
