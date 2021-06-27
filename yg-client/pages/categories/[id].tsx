38c4b83a58a9d92deefdf3a7a29756b7576fa2fe
Miguel <miguellealw@gmail.com>
2021-06-27 08:59:37 -0500

    Added search field in subscriptions section

--- yg-client/pages/categories/[id].tsx
+++ yg-client/pages/categories/[id].tsx
@@ -108,14 +108,36 @@ function SubscriptionsSection({ data }: { data?: CategoryResponse }) {
       {data?.category?.channels.length === 0 ? (
         <div className="text-sm text-gray-400">No channels in category</div>
       ) : (
-        <ul className="bg-white rounded-lg overflow-hidden shadow-lg p-8">
-          {data?.category?.channels.map((channel: Channel) => (
-            <CategorySubListItem
-              key={channel.yt_channel_id}
-              channel={channel}
+        <div
+          className="bg-white rounded-lg shadow-lg p-8 overflow-y-scroll"
+          style={{
+            maxHeight: "42rem",
+          }}
+        >
+          <div>
+            <label
+              htmlFor="searchSubs"
+              className="text-sm text-gray-400 font-bold block mb-1"
+            >
+              Search
+            </label>
+            <input
+              type="text"
+              id="searchSubs"
+              placeholder="Not working yet..."
+              className="w-full rounded-md mb-5 py-2"
+              style={{ textIndent: ".5rem" }}
             />
-          ))}
-        </ul>
+          </div>
+          <ul>
+            {data?.category?.channels.map((channel: Channel) => (
+              <CategorySubListItem
+                key={channel.yt_channel_id}
+                channel={channel}
+              />
+            ))}
+          </ul>
+        </div>
       )}
     </div>
   );
