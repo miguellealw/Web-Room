adac5f2504b5892f2089a83dcb274290a13a5fe9
Miguel <miguellealw@gmail.com>
2021-06-29 14:35:17 -0500

    made create page responsive

--- yg-client/pages/categories/create.tsx
+++ yg-client/pages/categories/create.tsx
@@ -20,7 +20,7 @@ const CreateCategory = () => {
     setIsError(false);
 
     try {
-      createCategory(value)
+      createCategory(value);
       router.replace("/categories");
     } catch (err) {
       setIsError(true);
@@ -31,24 +31,26 @@ const CreateCategory = () => {
     <AuthedLayout>
       <div className="py-10">
         <Link href="/categories" passHref>
-          <div className="flex mb-10 text-gray-400 hover:text-gray-600 cursor-pointer w-48">
+          <div className="text-sm lg:text-lg mb-5 hidden lg:flex items-center lg:mb-10 text-gray-400 hover:text-gray-600 cursor-pointer w-48">
             <ArrowNarrowLeftIcon className="w-6 h-6 mr-2" />
             Back to Categories
           </div>
         </Link>
 
-        <h1 className="text-5xl font-bold">Create Category</h1>
+        <h1 className="lg:pb-7 text-2xl lg:text-5xl font-bold">
+          Create Category
+        </h1>
 
         <form className="flex flex-col mt-10" onSubmit={handleCreateCategory}>
           <div>
-            <label htmlFor="name" className="text-lg font-bold block mb-1">
+            <label htmlFor="name" className="text-base lg:text-lg font-bold block mb-1">
               Category Name
             </label>
             <input
               type="text"
               id="name"
               placeholder="Cooking Channels"
-              className="w-full rounded-md p-2"
+              className="w-full rounded-md p-1 lg:p-2"
               onChange={(e) => {
                 setValue(e.target.value);
               }}
@@ -62,7 +64,7 @@ const CreateCategory = () => {
             </div>
           )}
 
-          <div className="mt-4">
+          <div className="mt-4 text-sm lg:text-base">
             <button className="bg-gray-800 hover:bg-gray-700 text-white px-5 py-1 rounded-md">
               Create
             </button>
