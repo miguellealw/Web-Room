c1154a9443b7abfb8a2d56fb065b5d4d52abcd46
Miguel <miguellealw@gmail.com>
2021-06-30 13:25:25 -0500

    style changes to drag preview

--- yg-client/pages/channels/SubscriptionDragPreview.tsx
+++ yg-client/pages/channels/SubscriptionDragPreview.tsx
@@ -1,5 +1,10 @@
+import { DuplicateIcon } from "@heroicons/react/outline";
+
 const SubscriptionDragPreview = ({ name }: { name: string }) => (
-  <div className="bg-white px-3 py-2 w-max rounded-sm">{name}</div>
+  <div className="bg-white px-3 py-2 w-max rounded-md text-sm font-bold flex items-center justify-center">
+    <DuplicateIcon className="w-5 h-5 mr-3" />
+    <span>{name}</span>
+  </div>
 );
 
 export default SubscriptionDragPreview;
