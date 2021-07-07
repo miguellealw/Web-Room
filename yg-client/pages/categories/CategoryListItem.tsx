d2c4e9ecca80cd4f8d4da8ff6028d48869b14b38
Miguel <miguellealw@gmail.com>
2021-07-07 02:39:16 -0500

    refactor + added dropdown to catsublistitem

--- yg-client/pages/categories/CategoryListItem.tsx
+++ yg-client/pages/categories/CategoryListItem.tsx
@@ -8,136 +8,53 @@ import React, { useState } from "react";
 import Dropdown from "../../components/Dropdown";
 import { Category } from "../api/types";
 
-type CategoryDropdownProps = {
-  isDropdownOpen: boolean;
-  categoryId: number;
-  categoryName: string;
-  setIsEditing: (value: boolean) => void;
-  setIsDropdownOpen: (value: boolean) => void;
-  handleDeleteCategory: (id: number, name: string) => void;
-};
-
-const CategoryDropdown: React.FC<CategoryDropdownProps> = ({
-  isDropdownOpen,
-  setIsDropdownOpen,
-  handleDeleteCategory,
-  categoryId,
-  categoryName,
-}) => {
-  return (
-    <>
-      <Dropdown isOpen={isDropdownOpen}>
-        <Dropdown.Item>
-          <Link href={`/categories/edit/${categoryId}`} passHref>
-            <a className="flex items-center w-full h-full">
-              <PencilAltIcon className="w-4 h-4 mr-1" />
-              Rename
-            </a>
-          </Link>
-        </Dropdown.Item>
-        <Dropdown.Item
-          handleClick={(e) => {
-            e.preventDefault();
-            handleDeleteCategory(categoryId, categoryName);
-            setIsDropdownOpen(false);
-          }}
-        >
-          <span className="flex items-center">
-            <FolderRemoveIcon className="w-4 h-4 mr-1" />
-            Delete
-          </span>
-        </Dropdown.Item>
-      </Dropdown>
-    </>
-  );
-};
-
 type CategoryListItemProps = {
   category: Category;
   handleDeleteCategory: (id: number, name: string) => void;
-  handleUpdateCategory: (id: number, value: string) => void;
 };
 
 const CategoryListItem: React.FC<CategoryListItemProps> = ({
   category,
   handleDeleteCategory,
-  handleUpdateCategory,
 }) => {
-  const [value, setValue] = useState<string>(category.name);
-  const [isEditing, setIsEditing] = useState<boolean>(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
     <Link href={`/categories/${category.id}`} passHref>
       <a>
         <li className="bg-white shadow-sm hover:shadow-md transition text-lg font-bold h-28 rounded-md flex justify-center items-center relative cursor-pointer">
-          {!isEditing && (
-            <DotsVerticalIcon
-              className="w-5 h-5 absolute text-gray-400 top-0 right-0 m-2 cursor-pointer hover:bg-gray-200 hover:text-black rounded-full"
-              onClick={(e) => {
+          <DotsVerticalIcon
+            className="w-5 h-5 absolute text-gray-400 top-0 right-0 m-2 cursor-pointer hover:bg-gray-200 hover:text-black rounded-full"
+            onClick={(e) => {
+              e.preventDefault();
+              setIsDropdownOpen(!isDropdownOpen);
+            }}
+          />
+          <span className="w-40 truncate text-center">{category.name}</span>
+
+          {/* Dropdown */}
+          <Dropdown isOpen={isDropdownOpen}>
+            <Dropdown.Item>
+              <Link href={`/categories/edit/${category.id}`} passHref>
+                <a className="flex items-center w-full h-full">
+                  <PencilAltIcon className="w-4 h-4 mr-1" />
+                  Rename
+                </a>
+              </Link>
+            </Dropdown.Item>
+            <Dropdown.Item
+              handleClick={(e) => {
                 e.preventDefault();
-                setIsDropdownOpen(!isDropdownOpen);
+                handleDeleteCategory(category.id, category.name);
+                setIsDropdownOpen(false);
               }}
-            />
-          )}
-
-          {isDropdownOpen && (
-            <CategoryDropdown
-              isDropdownOpen={isDropdownOpen}
-              setIsEditing={setIsEditing}
-              setIsDropdownOpen={setIsDropdownOpen}
-              handleDeleteCategory={handleDeleteCategory}
-              categoryId={category.id}
-              categoryName={category.name}
-            />
-          )}
-
-          {!isEditing ? (
-            <span className="w-40 truncate text-center">{category.name}</span>
-          ) : (
-            // TODO: make editing page like create
-            <div>
-              <input
-                type="text"
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-                className="rounded-md"
-                style={{
-                  textIndent: ".5rem",
-                }}
-                onClick={(e) => {
-                  e.preventDefault();
-                }}
-              />
-
-              {/* Buttons */}
-              <div>
-                <button
-                  type="submit"
-                  className="text-sm bg-gray-900 hover:bg-gray-600 text-white px-2 rounded"
-                  // disabled={value === ""}
-                  // disabled={true}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setIsEditing(false);
-                    handleUpdateCategory(category.id, value);
-                  }}
-                >
-                  Save
-                </button>
-
-                <button
-                  className="text-sm bg-red-500 hover:bg-red-400 text-white px-2 ml-2 rounded"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setIsEditing(false);
-                  }}
-                >
-                  Cancel
-                </button>
-              </div>
-            </div>
-          )}
+            >
+              <span className="flex items-center">
+                <FolderRemoveIcon className="w-4 h-4 mr-1" />
+                Delete
+              </span>
+            </Dropdown.Item>
+          </Dropdown>
         </li>
       </a>
     </Link>
--- yg-client/pages/categories/CategorySubListItem.tsx
+++ yg-client/pages/categories/CategorySubListItem.tsx
@@ -4,38 +4,7 @@ import { ExternalLinkIcon } from "@heroicons/react/outline";
 import { Channel } from "../api/types";
 import { DotsVerticalIcon, XIcon } from "@heroicons/react/outline";
 import { useCallback, useState } from "react";
-
-type CategoryDropdownProps = {
-  isDropdownOpen: boolean;
-  setIsDropdownOpen: (value: boolean) => void;
-  handleRemoveChannelFromCategory: () => void;
-};
-
-const Dropdown: React.FC<CategoryDropdownProps> = ({
-  isDropdownOpen,
-  setIsDropdownOpen,
-  handleRemoveChannelFromCategory,
-}) => {
-  return (
-    <>
-      {isDropdownOpen && (
-        <ul className="bg-white w-max absolute text-sm font-normal top-8 right-3 rounded-sm shadow-xl">
-          <li
-            className="py-2 px-4 hover:bg-gray-100 border border-gray-300 rounded-md cursor-pointer flex"
-            onClick={(e) => {
-              e.preventDefault();
-              handleRemoveChannelFromCategory();
-              setIsDropdownOpen(false);
-            }}
-          >
-            <XIcon className="w-5 h-5 mr-2" />
-            Remove from Category
-          </li>
-        </ul>
-      )}
-    </>
-  );
-};
+import Dropdown from "../../components/Dropdown";
 
 type CategorySubListItemProps = {
   channel: Channel;
@@ -61,7 +30,9 @@ export const MobileCategorySubListItem: React.FC<CategorySubListItemProps> = ({
               height={200}
             />
           </div>
-          <span className="text-sm w-16 text-center block truncate text-gray-500">{channel.name}</span>
+          <span className="text-sm w-16 text-center block truncate text-gray-500">
+            {channel.name}
+          </span>
         </div>
       )}
     </li>
@@ -80,13 +51,7 @@ const CategorySubListItem: React.FC<CategorySubListItemProps> = ({
   }, [channel, removeChannelFromCategory]);
 
   return (
-    <li className="py-5 bg-gray-100 mb-3 rounded-lg flex items-center pl-4 relative overflow-hidden">
-      <Dropdown
-        isDropdownOpen={isDropdownOpen}
-        setIsDropdownOpen={setIsDropdownOpen}
-        handleRemoveChannelFromCategory={handleRemoveChannelFromCategory}
-      />
-
+    <li className="py-5 bg-gray-100 mb-3 rounded-lg flex items-center pl-4 relative">
       {/* Thumbnail */}
       {!channel.yt_data ? (
         <div className="rounded-full w-14 h-14 bg-gray-500 overflow-hidden"></div>
@@ -105,18 +70,22 @@ const CategorySubListItem: React.FC<CategorySubListItemProps> = ({
       {/* Data */}
       <div className="ml-3">
         <DotsVerticalIcon
-          className="w-5 h-5 absolute text-gray-400 top-0 right-0 m-2 cursor-pointer"
+          // className="w-5 h-5 absolute text-gray-400 top-0 right-0 m-2 cursor-pointer"
+          className="w-5 h-5 absolute text-gray-400 top-0 right-0 m-2 cursor-pointer hover:bg-gray-200 hover:text-black rounded-full"
           onClick={(e) => {
             e.preventDefault();
-            e.stopPropagation();
             setIsDropdownOpen(!isDropdownOpen);
           }}
         />
-        <div className="font-bold text-lg max-w-48 truncate">{channel.name}</div>
+        {/* Name */}
+        <div className="font-bold text-lg max-w-48 truncate">
+          {channel.name}
+        </div>
 
+        {/* Subscribers */}
         {channel.yt_data &&
         !channel.yt_data.statistics.hiddenSubscriberCount ? (
-          <div className="text-sm">
+          <div className="text-sm text-gray-500">
             {numberWithCommas(channel.yt_data.statistics.subscriberCount)}{" "}
             Subscribers
           </div>
@@ -126,14 +95,31 @@ const CategorySubListItem: React.FC<CategorySubListItemProps> = ({
           </div>
         )}
 
-        <a
-          href={`https://www.youtube.com/channel/${channel.yt_channel_id}`}
-          // target="_blank"
-          className="text-xs text-gray-400 hover:underline flex mt-1"
-        >
-          Go to Channel
-          <ExternalLinkIcon className="w-4 h-4 ml-1" />
-        </a>
+        {/* Dropdown */}
+        <Dropdown isOpen={isDropdownOpen} tw_className="w-44">
+          <Dropdown.Item
+            handleClick={(e) => {
+              e.preventDefault();
+              handleRemoveChannelFromCategory();
+              setIsDropdownOpen(false);
+            }}
+          >
+            <span className="flex items-center">
+              <XIcon className="w-4 h-4 mr-1" />
+              Remove from Category
+            </span>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <a
+              href={`https://www.youtube.com/channel/${channel.yt_channel_id}`}
+              // target="_blank"
+              className="flex"
+            >
+              <ExternalLinkIcon className="w-4 h-4 mr-1" />
+              Go to Channel
+            </a>
+          </Dropdown.Item>
+        </Dropdown>
       </div>
     </li>
   );
