c9469aff1ee4ef129533b2faa5a01188dfcc19ce
Miguel <miguellealw@gmail.com>
2021-07-08 00:23:37 -0500

    Moved log out button to mobile nav

--- yg-client/modules/layouts/MobileNavigation.tsx
+++ yg-client/modules/layouts/MobileNavigation.tsx
@@ -1,19 +1,38 @@
-import { CollectionIcon, FolderIcon } from "@heroicons/react/outline";
+import {
+  CollectionIcon,
+  FolderIcon,
+  LogoutIcon,
+} from "@heroicons/react/outline";
 import {
   CollectionIcon as CollectionIconSolid,
   FolderIcon as FolderIconSolid,
 } from "@heroicons/react/solid";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { AuthApi } from "../../pages/api/auth";
+import useUser from "../../shared-hooks/useUser";
 
 const MobileNavigation = () => {
   const router = useRouter();
   const isCategories = router.pathname === "/categories";
   const isSubscriptions = router.pathname === "/channels";
+
+  // TODO: optimize this to not use useUser
+  const { mutateUser } = useUser();
+
+  const handleLogout = async () => {
+    mutateUser();
+    const api = new AuthApi();
+    api.setup();
+    await api.logout();
+    router.replace("/");
+    mutateUser();
+  };
+
   return (
     <nav className="w-full h-14 fixed left-0 bottom-0 bg-white z-10 block lg:hidden shadow-sm border">
-      <ul className="flex h-full">
-        <li className={`w-1/2 h-full  text-black`}>
+      <ul className="flex justify-around h-full">
+        <li className={`h-full  text-black`}>
           <Link href="/categories" passHref>
             <a className="h-full flex flex-col items-center justify-center">
               {isCategories ? (
@@ -25,7 +44,7 @@ const MobileNavigation = () => {
             </a>
           </Link>
         </li>
-        <li className={`w-1/2 h-full text-black`}>
+        <li className={`h-full text-black`}>
           <Link href="/channels" passHref>
             <a className="h-full flex flex-col items-center justify-center">
               {isSubscriptions ? (
@@ -36,6 +55,17 @@ const MobileNavigation = () => {
               <span className="text-xs">Subscriptions</span>
             </a>
           </Link>
+        </li>
+        <li className={`h-full text-black`}>
+          <button
+            data-tip="Log Out"
+            onClick={handleLogout}
+            // className="flex items-center my-3"
+            className="h-full flex flex-col items-center justify-center"
+          >
+            <LogoutIcon className="h-5 w-5" />
+            <span className="text-xs">Log Out</span>
+          </button>
         </li>
       </ul>
     </nav>
--- yg-client/modules/layouts/MobileTopNavBar.tsx
+++ yg-client/modules/layouts/MobileTopNavBar.tsx
@@ -6,7 +6,6 @@ const MobileTopNavBar = () => {
     <div className="w-full h-14 bg-white lg:hidden">
       <div className="h-full w-11/12 m-auto flex justify-between items-center">
         <LogoType />
-        <LogoutIcon className="h-5 w-5" />
       </div>
     </div>
   );
