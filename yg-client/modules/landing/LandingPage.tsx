fc2f7cde89c17ba8a89a64e08807b31fc51033ec
Miguel <miguellealw@gmail.com>
2021-07-09 02:59:38 -0500

    Combined 2 svgs to 1 + implemented login

--- yg-client/modules/landing/LandingPage.tsx
+++ yg-client/modules/landing/LandingPage.tsx
@@ -1,43 +1,83 @@
 import Head from "next/head";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../layouts/layout";
 import WaveSVG from "./WaveSVG";
-import WaveSVG2 from "./WaveSVG2";
 import Image from "next/image";
 import CategoriesImage from "../../public/categories-landing-image.png";
+import useUser from "../../shared-hooks/useUser";
+import { AuthApi } from "../../pages/api/auth";
+import { useRouter } from "next/router";
 
 const LoginForm: React.FC = () => {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  const { mutateUser } = useUser({
+    redirectTo: "/channels",
+    redirectIfFound: true,
+  });
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsError(false);
+    try {
+      const api = new AuthApi();
+      api.setup();
+      const response = await api.login(username, password);
+
+      if (response.kind === "ok") {
+        mutateUser();
+        router.push("/categories");
+      } else {
+        setIsError(true);
+      }
+    } catch (err) {
+      setIsError(true);
+    }
+  };
+
   return (
     <form
       action="post"
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
+      onSubmit={onSubmit}
       className="w-11/12 bg-white shadow-xl rounded-md p-5 relative -top-16 lg:-top-24"
     >
       <div className="lg:w-80 m-auto">
         <h3 className="font-bold text-3xl text-center mb-5">Log In</h3>
         <div className="flex flex-col text-xs mb-6">
-          <label htmlFor="username" className="uppercase font-bold">
+          <label htmlFor="username" className="uppercase font-bold mb-1">
             Username
           </label>
           <input
             type="text"
             id="username"
-            className="rounded-md border-2 border-gray-200 focus:border-red-400 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+            className="rounded-md border-2 border-gray-200 focus:border-red-400 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 text-sm"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setUsername(e.target.value);
+            }}
           />
         </div>
 
         <div className="flex flex-col text-xs">
-          <label htmlFor="password" className="uppercase font-bold">
+          <label htmlFor="password" className="uppercase font-bold mb-1">
             Password
           </label>
           <input
             type="password"
             id="password"
-            className="rounded-md border-2 border-gray-200 focus:border-red-400 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+            className="rounded-md border-2 border-gray-200 focus:border-red-400 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 text-sm"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setPassword(e.target.value);
+            }}
           />
+          {isError && (
+            <div className="font-bold text-sm text-red-600 mt-2">
+              There&apos;s an error while login, try again!
+            </div>
+          )}
           {/* TODO: change to link */}
           <span className="text-xs text-gray-400 underline mt-2">
             Forgot My Password
@@ -57,7 +97,7 @@ const LoginForm: React.FC = () => {
 export const LandingPage: React.FC = () => {
   return (
     <Layout>
-      <div className="flex flex-col justify-center items-center w-full min-h-screen">
+      <div className="flex flex-col justify-center items-center w-full">
         <Head>
           <title>YouTube+</title>
           <meta
@@ -67,8 +107,13 @@ export const LandingPage: React.FC = () => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        {/* <WaveSVG className="absolute left-0 right-0 top-0 bottom-0 bg-blue-200 w-full" width="1439" height="522"/>
-        <WaveSVG2 className="absolute left-0 right-0"/> */}
+        <WaveSVG
+          className="absolute top-56 left-0 right-0"
+          width="100%"
+          height="100vh"
+          svgWidth="1439"
+          svgHeight="522"
+        />
         <main className="min-h-screen flex-1 flex flex-col justify-center items-center z-10">
           <h1 className="text-2xl lg:text-6xl font-bold mt-24">
             A Better YouTube Experience
--- yg-client/modules/landing/WaveSVG.tsx
+++ yg-client/modules/landing/WaveSVG.tsx
@@ -1,20 +1,27 @@
-function WaveSVG({width, height, ...props}) {
+// https://stackoverflow.com/questions/19484707/how-can-i-make-an-svg-scale-with-its-parent-container
+function WaveSVG({ width, height, svgWidth, svgHeight, ...props }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      // width="1439"
-      // height="522"
-			// viewBox="0 0 30 522"
-			viewBox={`0 0 ${width} ${height}`}
+      width={width}
+      height={height}
+      viewBox={`0 0 ${svgWidth} ${svgHeight}`}
       fill="none"
-			preserveAspectRatio="xMidYMid meet"
-			{...props}
+      // preserveAspectRatio="xMidYMid meet"
+      {...props}
     >
       <path
         fill="#F31E1E"
         fillOpacity="0.2"
         fillRule="evenodd"
-        d="M0 52.481l60-13.12C120 26.241 240 0 360 0s240 26.24 360 106.837c120 80.596 240 215.548 360 256.783 120 41.236 240-13.12 300-41.235l60-26.241V701H0V52.481z"
+        d="M1 122.481l60-13.12C121 96.241 241 70 361 70s240 26.24 360 106.837c120 80.596 240 215.548 360 256.783 120 41.236 240-13.12 300-41.235l60-26.241V771H1V122.481z"
+        clipRule="evenodd"
+      ></path>
+      <path
+        fill="#FF4848"
+        fillOpacity="0.44"
+        fillRule="evenodd"
+        d="M-7 354.572l39.82-26.381c41.027-26.381 120.667-79.143 201.513-52.762 80.847 26.381 160.487 131.904 241.334 79.143C556.513 301.81 636.153 90.762 717 24.81 797.847-41.142 877.487 38 958.333 103.953c80.847 65.952 160.487 118.714 241.337 105.524 80.84-13.191 160.48-92.334 201.51-131.905L1441 38v554H-7V354.572z"
         clipRule="evenodd"
       ></path>
     </svg>
