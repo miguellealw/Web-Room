7754888a37cb71583a2f927ce2dbce4f0c5adc2b
Miguel <miguellealw@gmail.com>
2021-07-10 23:38:59 -0500

    styling change on nav

--- yg-client/modules/layouts/layout.tsx
+++ yg-client/modules/layouts/layout.tsx
@@ -6,7 +6,7 @@ import LogoType from "../../components/LogoType";
 const Layout: FC = ({ children }) => {
   return (
     <div className="font-inter">
-      <nav className="w-full py-5 absolute flex justify-center z-20">
+      <nav className="w-full py-8 absolute flex justify-center z-20">
         <Link href="/" passHref>
           <a className="flex items-center">
             <LogoType />
