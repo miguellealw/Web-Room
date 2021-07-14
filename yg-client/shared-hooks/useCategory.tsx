2495502b4de751665fde3e8e7716f9bdab9e8b97
Miguel <miguellealw@gmail.com>
2021-07-13 23:30:43 -0500

    added back swr cache checks

--- yg-client/shared-hooks/useCategory.tsx
+++ yg-client/shared-hooks/useCategory.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useMemo } from "react";
-import { CategoryApi } from "../pages/api/categories";
+import { mutate } from "swr";
+import { CategoryApi, CategoryResponse } from "../pages/api/categories";
 
 function useCategory(id: number) {
   const api = useMemo(() => new CategoryApi(), []);
@@ -7,15 +8,60 @@ function useCategory(id: number) {
 
   const memoAddChannelToCategory = useCallback(
     async (channelName: string, channelId: string) => {
+      // Update ui
+      mutate(
+        `/api/v1.0/users/current_user/categories/${id}`,
+        (data: CategoryResponse) => {
+          if (!data || !data.category) return;
+
+          return {
+            ...data,
+            category: {
+              ...data.category,
+              channels: [
+                ...data.category.channels,
+                {
+                  name: channelName,
+                  yt_channel_id: channelId,
+                },
+              ],
+            },
+          };
+        },
+        false
+      );
+
       await api.addChannelToCategory(id, channelName, channelId);
+
+      // Revalidate cache
+      mutate(`/api/v1.0/users/current_user/categories/${id}`);
     },
     [api, id]
   );
 
   const memoRemoveChannelFromCategory = useCallback(
     async (channelName: string, channelId: string) => {
-      // await removeChannelFromCategory(api, id, channelName, channelId);
+      mutate(
+        `/api/v1.0/users/current_user/categories/${id}`,
+        (data: CategoryResponse) => {
+          if (!data || !data.category) return;
+
+          const channelsToKeep = data.category.channels.filter(
+            (channel) => channel.yt_channel_id !== channelId
+          );
+
+          return {
+            ...data,
+            category: { ...data.category, channels: [...channelsToKeep] },
+          };
+        },
+        false
+      );
+
       await api.removeChannelFromCategory(id, channelName, channelId);
+
+      // revalidate cache
+      mutate(`/api/v1.0/users/current_user/categories/${id}`);
     },
     [api, id]
   );
