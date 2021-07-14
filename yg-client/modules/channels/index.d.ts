cba8cddbda58485ab80dc7e897383ba3ce82ff8f
Miguel <miguellealw@gmail.com>
2021-07-14 18:36:25 -0500

    some type changes

--- yg-client/modules/channels/index.d.ts
+++ yg-client/modules/channels/index.d.ts
@@ -1,12 +1,15 @@
-export type ResourceLinks = {
-  self: string;
-  collection: string;
-};
+import { youtube_v3 } from "../youtube";
+
+// TODO: implement yt_data type
+export type YT_Data = any
 
 export type Channel = {
   name: string;
   yt_channel_id: string;
-  _links: ResourceLinks;
+  _links: {
+    self: string;
+    collection: string;
+  };
   categories: number[];
-  yt_data: any;
+  yt_data: YT_Data;
 };
