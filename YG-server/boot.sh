1feb0cabd474d33f3e6f7fb39f30e8a9d2f1fb0d
Miguel <miguellealw@gmail.com>
2021-08-05 00:23:50 -0500

    created boot script

---
+++ YG-server/boot.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Script by Miguel Grinberg
+
+# this script is used to boot a Docker container
+source venv/bin/activate
+
+while true; do
+	# Apply db migrations
+	flask db upgrade
+	if [[ "$?" == "0" ]]; then
+			break
+	fi
+	echo Deploy command failed, retrying in 5 secs...
+	sleep 5
+done
+
+# flask translate compile
+
+# exec ... - will trigger the process running the script to be replaced with the 
+# command given. Important for docker since the life of the container is the first process it sees
+
+# access-logfile and error-logfile - sends log to standard output that they are stored as logs by Docker.
+exec gunicorn -b :5000 --access-logfile - --error-logfile - youtubebox:app
